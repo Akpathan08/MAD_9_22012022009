cf4a75219e3075ebb8547946b4df9eb7c7c2b104
ANISH <anishkhanypathan@gmailcom>
2023-11-28 14:46:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_9_22012022009/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mad_9_22012022009
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.mad_9_22012022009.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var alarmimages_frameByFrameAnimation : AnimationDrawable
+    lateinit var heart_frameByFrameAnimation : AnimationDrawable
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgAlarm.setBackgroundResource(R.drawable.alarm_image_list)
+        binding.imgHeart.setBackgroundResource(R.drawable.heart_image_list)
+        alarmimages_frameByFrameAnimation = binding.imgAlarm.background as AnimationDrawable
+        heart_frameByFrameAnimation = binding.imgHeart.background as AnimationDrawable
+    }
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if(hasFocus){
+            alarmimages_frameByFrameAnimation.start()
+            heart_frameByFrameAnimation.start()
+        }
+    }}
